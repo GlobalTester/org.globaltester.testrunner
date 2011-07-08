a01f731c035bef05f536b96da6884864cb658ee1
Alexander May <alexander.may@hjp-consulting.com>
2011-07-08 07:45:39 +0000

    initial import
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testrunner/trunk@173 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/testrunner/testframework/TestCaseExecution.java
@@ -0,0 +1,5 @@
+package org.globaltester.testrunner.testframework;
+
+public class TestCaseExecution extends TestExecution {
+
+}
---
+++ src/org/globaltester/testrunner/testframework/TestExecution.java
@@ -0,0 +1,12 @@
+package org.globaltester.testrunner.testframework;
+
+/**
+ * This abstract class defines all common methods of individually executable
+ * test portions (e.g. test cases or test suites)
+ * 
+ * @author amay
+ * 
+ */
+public abstract class TestExecution {
+
+}
