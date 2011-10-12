ca452f32c2a80cb14943a7369664a3a0ab1b0d5a
Alexander May <alexander.may@hjp-consulting.com>
2011-10-12 15:11:44 +0000

    implement compund results
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testrunner/trunk@283 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/testrunner/testframework/Result.java
+++ src/org/globaltester/testrunner/testframework/Result.java
@@ -1,30 +1,28 @@
 package org.globaltester.testrunner.testframework;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
- * Represents the test result of an executable unit, e.g. of a TestCase a
- * TestStep or even only a single line of code.
+ * Represents the test result of an executable unit, e.g. of a
+ * TestCaseExecution.
+ * 
+ * The result may consist of several subResults. In this case the overall result
+ * will reflect the worst subResult. This behavior may be changed by subclasses.
  * 
  * @author amay
  * 
  */
 public class Result {
-	
 
 	public enum Status {
-		PASSED,
-		FAILURE,
-		WARNING,
-		NOT_APPLICABLE,
-		ABORTED,
-		SKIPPED,
-		RESUMED,
-		UNDEFINED;
-		
+		PASSED, FAILURE, WARNING, NOT_APPLICABLE, UNDEFINED;
+
 	}
 
 	private Status status; // status this result represents
 	private String comment; // comment associated with this result
+	private ArrayList<Result> subResults = new ArrayList<Result>();
 
 	public Result(Status newStatus) {
 		this.status = newStatus;
@@ -41,7 +39,40 @@ public class Result {
 	public Status getStatus() {
 		return status;
 	}
-	
-	
+
+	/**
+	 * Add a new subResult and rebuild the overall status
+	 * 
+	 * @param result
+	 */
+	public void addSubResult(Result subResult) {
+		subResults.add(subResult);
+		rebuildStatus();
+	}
+
+	/**
+	 * Recalculate the overall status of this result from the subResults.
+	 * 
+	 * By default this returns the worst status given by an sub result.
+	 */
+	private void rebuildStatus() {
+		Status tmpStatus = Status.PASSED;
+
+		//search worst status in sub results
+		Iterator<Result> subResultIter = subResults.iterator();
+		while (subResultIter.hasNext()) {
+			Result curResult = (Result) subResultIter.next();
+			if (curResult.getStatus() == Status.WARNING) {
+				tmpStatus = Status.WARNING;
+			} else if (curResult.getStatus() == Status.FAILURE) {
+				tmpStatus = Status.FAILURE;
+				break;
+			}
+		}
+		
+		//change status of this result
+		status = tmpStatus;
+
+	}
 
 }
