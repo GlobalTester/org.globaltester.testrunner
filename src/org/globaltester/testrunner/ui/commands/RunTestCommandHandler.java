7280901c1525c0a1ce36a77644fd8607723701b2
Lothar Bornemann <lothar.bornemann@hjp-consulting.com>
2012-02-08 15:00:38 +0000

    add Run Testcase action for the menu //ToDo finalize run(IAction action) method
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testrunner.ui/trunk@460 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/testrunner/ui/commands/RunTestCommandHandler.java
+++ src/org/globaltester/testrunner/ui/commands/RunTestCommandHandler.java
@@ -6,12 +6,22 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
 import org.globaltester.testrunner.GtTestCampaignProject;
 
-public class RunTestCommandHandler extends AbstractHandler {
+public class RunTestCommandHandler extends AbstractHandler implements IWorkbenchWindowActionDelegate {
 
+	
+	/**
+	 * Constructor of RunTestCommandHandler
+	 */
+	public RunTestCommandHandler() {
+	}
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		// check for dirty files and save them
@@ -50,6 +60,24 @@ public class RunTestCommandHandler extends AbstractHandler {
 		CreateTestCampaignCommandHandler.openInEditor(campaingProject);
 
 		return null;
+	}
+
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
