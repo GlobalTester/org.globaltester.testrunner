c2ae238227f5b18202d758503c2a1579457dbba2
Liane Velten <liane.velten@hjp-consulting.com>
2012-03-08 16:12:17 +0000

    moved preferences "single logfile for each testcase" back to loggin; cleaned up testrunner preferences
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testrunner/trunk@781 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/testrunner/preferences/PreferenceConstants.java
@@ -0,0 +1,18 @@
+package org.globaltester.testrunner.preferences;
+/**
+ * This class stores the properties of the plugin Constant definitions for
+ * plug-in preferences
+ * 
+ * @author amay
+ * 
+ */
+
+public class PreferenceConstants {
+
+	//constants for double click in
+	public static final String P_DOUBLECLICKRESULTVIEW =  "Double click in TestCampaign editor opens";
+	public static final String TEST_CASE = "Test Case";	
+	public static final String LOG_FILE = "Log File";
+	
+
+}
---
+++ src/org/globaltester/testrunner/preferences/PreferenceInitializer.java
@@ -0,0 +1,41 @@
+package org.globaltester.testrunner.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.globaltester.testrunner.Activator;
+import org.osgi.service.prefs.BackingStoreException;
+import org.osgi.service.prefs.Preferences;
+
+/**
+ * This class stores the properties of the plugin Class used to initialize
+ * default preference values
+ * 
+ * @author amay
+ * 
+ */
+
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * Use this to store plugin preferences For meaning of each preference look
+	 * at PreferenceConstants.java
+	 */
+	public void initializeDefaultPreferences() {
+
+		Preferences preferences = new DefaultScope()
+				.getNode(Activator.PLUGIN_ID);
+		
+		
+		//if TestCase or log file should open on double click
+		preferences.putInt(PreferenceConstants.P_DOUBLECLICKRESULTVIEW, 0);
+		
+		// Force the application to save the preferences
+		try {
+			preferences.flush();
+		} catch (BackingStoreException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
